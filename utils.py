045e4638d8d2456a0a70a347edc70ffdde1b0495
Brian R. Isett <brian.isett@gmail.com>
2021-01-06 11:58:05 -0500

    Implement a custom warning function that prints colored text output using colorama (placeholder).

---
+++ utils.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  6 11:54:56 2021
+
+@author: brian
+"""
+import colorama 
+
+def warning(message):
+    #Use colorama to print a warning (not yet implemented)
+    print('colorama')
